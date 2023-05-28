ddfd309477a3802936220d97837e0c07b253f868
rbtessier <r.b.tessier@gmail.com>
2023-05-28 15:56:26 -0600

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ model = LogisticRegression(max_iter=200)
 model.fit(X_train, y_train)
 
 # Compute SHAP values
-explainer = shap.LinearExplainer(model)
+explainer = shap.LinearExplainer(model, X_train)
 shap_values = explainer.shap_values(X)
 
 st.title('Diabetes Prediction App')
